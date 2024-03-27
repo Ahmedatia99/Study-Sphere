b8126c69ee8fa5553cffd8a3e86898ac528830a5
Ahmedatia99 <atyaa629@gmail.com>
2024-03-27 15:43:29 +0200

    add spounser section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../component/Header";
+import Sponser from "../component/Sponser"
 
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Sponser />
     </>
   );
 }

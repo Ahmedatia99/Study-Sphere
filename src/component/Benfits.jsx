8081c67c0e5653bdf252640ff39228a6d1a6e4e1
Ahmedatia99 <atyaa629@gmail.com>
2024-03-28 04:24:59 +0200

    starting in beefits section

--- src/component/Benfits.jsx
+++ src/component/Benfits.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { Button } from "./atoms/Button/Button.tsx";
 export default function Benfits() {
   return (
     <main>
-        <section>
-            
-        </section>
+      <section className="container mx-auto flex justify-center items-center">
+        <div className="flex">
+          <div>
+            <h2 className="desktop:text-5xl font-semibold text-textcook">
+              Benefits
+            </h2>
+            <p className="desktop:text-lg text-textcook">
+              Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam
+              eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac
+              cum eget habitasse in velit fringilla feugiat senectus in.
+            </p>
+          </div>
+          <Button variant="second" action="View All" />
+        </div>
+      </section>
     </main>
-  )
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "../component/Header";
-import Sponser from "../component/Sponser"
-
+import Sponser from "../component/Sponser";
+import Benfits from "../component/Benfits";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Sponser />
+      <Benfits />
     </>
   );
 }
